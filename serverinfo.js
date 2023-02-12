468869667b1bb0cf4bda8d34f5626fe3b9c5a1d4
Flaw5252 <94564997+Flaw5252@users.noreply.github.com>
2023-02-11 23:52:32 -0500

    Update serverinfo.js

--- serverinfo.js
+++ serverinfo.js
@@ -1,1 +1,22 @@
 
+const {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    SlashCommandBuilder,
+    EmbedBuilder,
+    ComponentType,
+    StringSelectMenuBuilder,
+  } = require("discord.js");
+
+const noblox = require("noblox.js")
+const Discord = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("serverinfo")
+    .setDescription("Displays information about mano servers"),
+  async execute(interaction, client) {
+    const universeInfo = await noblox.getUniverseInfo([ 4793176968 ])
+    }
+}
